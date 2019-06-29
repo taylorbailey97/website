f97d19cf87efbabff49c3ce71ab75e43217818e0
taylorbailey97 <36019252+taylorbailey97@users.noreply.github.com>
2019-06-29 09:27:17 -0600

    added site

---
+++ frontend/web/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <h1>Nails by Bailey</h1>
+    <ul>
+      <li><a href="">FaceBook</a></li>
+      <li><a href="">Twitter</a></li>
+      <li><a href="">Instagram</a></li>
+    </ul>
+</footer>
---
+++ frontend/web/header.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="./styles/styles.css"
+<header>
+  <img
+    src="../../assets/images/test_flowers.png"
+    alt="flowers"
+  />
+  <div class="brand"><a href="">
+    <h1>Nails</h1>
+    <p>BY BAILEY</p>
+    </a>
+  </div>
+
+  <ul class="header-nav">
+    <li ><a href="index.php">Home</a></li>
+    <li ><a href="gallery.php">Gallery</a></li>
+    <li ><a href="services.php">Services</a></li>
+    <li ><a href="schedule.php">Schedule</a></li>
+    <li ><a class="Contact" href="contact.php">Contact</a></li>
+  </ul>
+</header>
